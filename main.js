85b4a8bb7261e0037dbc316d344de8d58df865aa
Brian Richie Sr <brianrichiesr@gmail.com>
2023-09-19 09:01:47 -0700

    complete lab

--- main.js
+++ main.js
@@ -3,9 +3,35 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const heartIcons = document.querySelectorAll(".like-glyph");
+const errorModal = document.querySelector("#modal");
+const modalMessage = document.querySelector("#modal-message");
 
+const serverResults = (e) => {
+  mimicServerCall()
+  .then(() => {
+    if (e.target.textContent === EMPTY_HEART) {
+      e.target.textContent = FULL_HEART;
+      e.target.classList.add("activated-heart");
+    } else {
+      e.target.textContent = EMPTY_HEART;
+      e.target.classList.remove("activated-heart");
+    }
+  })
+  .catch(err => {
+    modalMessage.textContent = err;
+    errorModal.classList.remove("hidden");
+    setTimeout(() => {
+      modalMessage.textContent = "";
+      errorModal.classList.add("hidden");
+    }, 3000)
 
+  })
+}
 
+heartIcons.forEach(item => {
+  item.addEventListener("click", serverResults)
+})
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
